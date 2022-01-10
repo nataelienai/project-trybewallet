b290de28da69f51c963c3ace0ea13670cc6f83d8
Natã Elienai <nata.elienai@gmail.com>
2022-01-10 19:17:30 -0300

    feat(forms): add a login form to the login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,49 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('Submit!');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <form onSubmit={ this.handleSubmit }>
+        <input
+          type="text"
+          name="email"
+          value={ email }
+          data-testid="email-input"
+          onChange={ this.handleInputChange }
+        />
+        <input
+          type="password"
+          name="password"
+          value={ password }
+          data-testid="password-input"
+          onChange={ this.handleInputChange }
+        />
+        <button type="submit">Entrar</button>
+      </form>
+    );
   }
 }
 
