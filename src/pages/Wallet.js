b4697ef9066cee241c30304b9d9d8cf99f86e6ec
Natã Elienai <nata.elienai@gmail.com>
2022-01-11 20:22:37 -0300

    feat: add a table that displays added expenses

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -141,6 +141,21 @@ class Wallet extends React.Component {
           </label>
           <button type="submit">Adicionar despesa</button>
         </form>
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+        </table>
       </div>
     );
   }
