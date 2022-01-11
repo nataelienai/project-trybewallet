151e54af003abbdc84a35baada30f4f27b24f909
Natã Elienai <nata.elienai@gmail.com>
2022-01-11 18:17:03 -0300

    feat(redux): add API-requested currencies and user expenses to app state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,22 @@
 export const LOGIN = 'LOGIN';
+export const ADD_EXPENSE = 'ADD_EXPENSE';
+export const GET_CURRENCIES = 'GET_CURRENCIES';
 
 export const login = (payload) => ({ type: LOGIN, payload });
+export const addExpense = (payload) => ({ type: ADD_EXPENSE, payload });
+export const getCurrencies = (payload) => ({ type: GET_CURRENCIES, payload });
+
+export const saveExpense = (expense) => async (dispatch) => {
+  const response = await fetch('https://economia.awesomeapi.com.br/json/all');
+  const exchangeRates = await response.json();
+  delete exchangeRates.USDT;
+  expense.exchangeRates = exchangeRates;
+  dispatch(addExpense(expense));
+};
+
+export const fetchCurrencies = () => async (dispatch) => {
+  const response = await fetch('https://economia.awesomeapi.com.br/json/all');
+  const exchangeRates = await response.json();
+  delete exchangeRates.USDT;
+  dispatch(getCurrencies(Object.keys(exchangeRates)));
+};
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,21 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { ADD_EXPENSE, GET_CURRENCIES } from '../actions';
+
+const INITIAL_STATE = {
+  expenses: [],
+  currencies: [],
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case ADD_EXPENSE:
+    return { ...state, expenses: [...state.expenses, action.payload] };
+
+  case GET_CURRENCIES:
+    return { ...state, currencies: action.payload };
+
+  default:
+    return state;
+  }
+};
+
+export default wallet;
