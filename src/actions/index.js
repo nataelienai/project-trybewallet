6d1097a6e649fe0d05f7aa9b4c05225991bedc2a
Natã Elienai <nata.elienai@gmail.com>
2022-01-12 17:19:53 -0300

    feat(redux): create an action to edit a stored expense

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 export const LOGIN = 'LOGIN';
 export const ADD_EXPENSE = 'ADD_EXPENSE';
+export const EDIT_EXPENSE = 'EDIT_EXPENSE';
 export const DELETE_EXPENSE = 'DELETE_EXPENSE';
 export const GET_CURRENCIES = 'GET_CURRENCIES';
 
@@ -10,6 +11,11 @@ export const login = (emailAddress) => ({
 
 export const addExpense = (expense) => ({
   type: ADD_EXPENSE,
+  payload: expense,
+});
+
+export const editExpense = (expense) => ({
+  type: EDIT_EXPENSE,
   payload: expense,
 });
 
