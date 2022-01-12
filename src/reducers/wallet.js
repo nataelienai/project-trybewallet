77d6ae77cda3b5b7d2cd4b206c7fb05e3420d56c
Natã Elienai <nata.elienai@gmail.com>
2022-01-12 17:33:47 -0300

    feat(redux): add case in wallet reducer for action that edits expenses

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,4 +1,4 @@
-import { ADD_EXPENSE, DELETE_EXPENSE, GET_CURRENCIES } from '../actions';
+import { ADD_EXPENSE, EDIT_EXPENSE, DELETE_EXPENSE, GET_CURRENCIES } from '../actions';
 
 const INITIAL_STATE = {
   expenses: [],
@@ -10,14 +10,22 @@ const wallet = (state = INITIAL_STATE, action) => {
   case ADD_EXPENSE:
     return { ...state, expenses: [...state.expenses, action.payload] };
 
-  case GET_CURRENCIES:
-    return { ...state, currencies: action.payload };
+  case EDIT_EXPENSE:
+    return {
+      ...state,
+      expenses: state.expenses.map((expense) => (
+        expense.id === action.payload.id ? action.payload : expense
+      )),
+    };
 
   case DELETE_EXPENSE:
     return {
       ...state,
       expenses: state.expenses.filter((expense) => expense.id !== action.payload),
     };
+
+  case GET_CURRENCIES:
+    return { ...state, currencies: action.payload };
 
   default:
     return state;
