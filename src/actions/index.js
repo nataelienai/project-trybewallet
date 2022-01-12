f1ad94b6fa45c5e33ec5776aaca6eab968453380
Natã Elienai <nata.elienai@gmail.com>
2022-01-12 12:45:52 -0300

    feat(redux): create an action to delete a stored expense

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,27 @@
 export const LOGIN = 'LOGIN';
 export const ADD_EXPENSE = 'ADD_EXPENSE';
+export const DELETE_EXPENSE = 'DELETE_EXPENSE';
 export const GET_CURRENCIES = 'GET_CURRENCIES';
 
-export const login = (payload) => ({ type: LOGIN, payload });
-export const addExpense = (payload) => ({ type: ADD_EXPENSE, payload });
-export const getCurrencies = (payload) => ({ type: GET_CURRENCIES, payload });
+export const login = (emailAddress) => ({
+  type: LOGIN,
+  payload: emailAddress,
+});
+
+export const addExpense = (expense) => ({
+  type: ADD_EXPENSE,
+  payload: expense,
+});
+
+export const deleteExpense = (expenseId) => ({
+  type: DELETE_EXPENSE,
+  payload: expenseId,
+});
+
+export const getCurrencies = (currencies) => ({
+  type: GET_CURRENCIES,
+  payload: currencies,
+});
 
 export const saveExpense = (expense) => async (dispatch) => {
   const response = await fetch('https://economia.awesomeapi.com.br/json/all');
